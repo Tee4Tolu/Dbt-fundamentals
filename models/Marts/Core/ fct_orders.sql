d999fc8d164fdde78a7d76caafb4ebccb4eb447f
Tele Med <tolulopeabolarin94@gmail.com>
2022-02-23 14:45:24 +0000

    Created fct_orders table

---
+++ models/Marts/Core/ fct_orders.sql
@@ -0,0 +1,28 @@
+with orders as (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments' )}}
+) ,
+
+order_payments as (
+    select 
+        orderid,
+        status,
+        sum(case when status = 'success' then amount end) as amount
+    from payments 
+    group by orderid,status
+),
+
+final as ( select
+   orders.order_id,
+   orders.customer_id,
+   orders.order_date,
+   order_payments.status,
+   coalesce(order_payments.amount,0) as amount
+   from orders
+   left join order_payments
+   on order_payments.orderid = orders.order_id
+)
+select * from final
