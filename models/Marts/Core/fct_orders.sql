c2e3722cbc8c9683904db5cecc81c6ab560c85f6
Tele Med <tolulopeabolarin94@gmail.com>
2022-02-23 13:19:37 +0000

    Created fact tabble using payment and orders table

--- models/Marts/Core/fct_orders.sql
+++ models/Marts/Core/fct_orders.sql
@@ -1,5 +1,28 @@
-select 
-    id as Customer_id,
-    orderid as Order_id,
-    amount 
-from `dbt-tutorial.stripe.payment`;
+with orders as (
+    select * from {{ref('stg_orders')}}
+),
+
+payment as (
+    select * from {{ref('stg_payments')}}
+),
+
+payment_order as (
+    select 
+        order_id,
+        sum(case when status = 'success' then amount end ) amount
+    from payment
+    group by order_id
+
+),
+
+final as (
+    select 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (payment_order,0) as amount
+    from orders
+    left join payment_order
+    on orders.orders_id = payment_order.orders_id 
+)
+select * from final
