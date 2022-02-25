69e382d62668a363929ff1b76b88f1bf77ada0e9
Tele Med <tolulopeabolarin94@gmail.com>
2022-02-25 07:52:59 +0000

    Rechecked everything

--- models/Marts/Core/dim_customers.sql
+++ models/Marts/Core/dim_customers.sql
@@ -10,11 +10,11 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as recent_order_date,
         count(order_id) as Total_orders
+        
     
     from orders
     group by 1
 ),
-
 final as (
         select 
             customers.customer_id,
@@ -22,11 +22,11 @@ final as (
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.recent_order_date,
-            coalesce(customer_orders.Total_orders,0) as Total_orders
+            coalesce(customer_orders.Total_orders,0) as Total_orders,
+            
         from customers
         left join customer_orders 
         on customers.customer_id = customer_orders.customer_id
-            
 )
 select * from final
     
