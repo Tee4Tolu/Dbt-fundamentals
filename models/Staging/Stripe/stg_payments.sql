9b2e6682c9733c4747c34368f59f57060f6f28cf
Tele Med <tolulopeabolarin94@gmail.com>
2022-02-25 08:09:43 +0000

    Created stg_payments table

---
+++ models/Staging/Stripe/stg_payments.sql
@@ -0,0 +1,8 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    amount/100 as amount,
+    status as status,
+    created as date
+from `dbt-tutorial.stripe.payment`
