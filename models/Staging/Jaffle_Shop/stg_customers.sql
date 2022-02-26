5a26ba97d87bf4ad188ffcabecc1a7cf4670e1d6
Tele Med <tolulopeabolarin94@gmail.com>
2022-02-26 14:39:14 +0000

    Added source file to staging

--- models/Staging/Jaffle_Shop/stg_customers.sql
+++ models/Staging/Jaffle_Shop/stg_customers.sql
@@ -3,6 +3,6 @@ with customers as
         select id as customer_id,
         first_name,
         last_name
-    from `dbt-tutorial.jaffle_shop.customers`
+    from {{source ('jaffle_shop','customers')}}
     )
 select * from customers
